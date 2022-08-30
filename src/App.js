30c37af85d2e348f7943d558fdac04663d41e9ca
RomanVishnevetskyi <vrv080289@gmail.com>
2022-08-30 19:36:05 +0300

    create counter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import './index.scss';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <div>
-        <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
-      </div>
-    </div>
-  );
+    const [counter, setCounter] = useState(0);
+    const increment = () => {
+        setCounter(prevState => prevState + 1);
+    }
+    const decrement = () => {
+        setCounter(prevState => prevState - 1);
+    }
+    const reset = () => {
+        setCounter(0);
+    }
+    return (
+        <div className="App">
+            <div>
+                <h2>Счетчик:</h2>
+                <h1>{counter}</h1>
+                <button className="minus" onClick={decrement}>- Минус</button>
+                <button className="plus" onClick={increment}>Плюс +</button>
+                <button className="reset" onClick={reset}>Сброс</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
